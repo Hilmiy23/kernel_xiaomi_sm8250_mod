1983b10825055d96bdfd78f51ebebbcb564ef65c
Hilmiy <hilmizh23@gmail.com>
2025-09-07 13:04:31 +0700

    block: enable blk-mq and use NONE as default

--- block/elevator.c
+++ block/elevator.c
@@ -996,7 +996,7 @@ int elevator_init_mq(struct request_queue *q)
 		if (!e)
 			goto out;
 	} else {
-		e = elevator_get(q, "mq-deadline", false);
+		e = elevator_get(q, "none", false);
 		if (!e)
 			goto out;
 	}
