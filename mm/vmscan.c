7d13892edee4e553c3e7182266498d99fda26530
Hilmiy <hilmizh23@gmail.com>
2025-10-05 00:16:00 +0700

    mm/mglru: replace thp_nr_pages() with compound_order

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4442,11 +4442,7 @@ retry:
 
 		/* retry pages that may have missed rotate_reclaimable_page() */
 		list_move(&page->lru, &clean);
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE
-    sc->nr_scanned -= thp_nr_pages(page);
-#else
-    sc->nr_scanned -= 1;  /* fallback as we don't have THP present */
-#endif
+    sc->nr_scanned -= compound_order(page);
 
 	}
 
